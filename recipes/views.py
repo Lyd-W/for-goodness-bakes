8cbcaa1d32fbb9d77631e0a354eae1cc25cf3a61
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-01-14 20:06:48 +0000

    Create recipe_detail and set url pattern

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
 
 # Create your views here.
-
 class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by("created_on")
     context_object_name = "recipes"
     template_name = "recipes/index.html"
     paginate_by = 6
+    
+def recipe_detail(request, slug):
+    queryset = Recipe.objects.filter(status=1)
+    recipe = get_object_or_404(queryset, slug=slug)
+    
+    return render(
+        request,
+        "recipes/recipe_detail.html",
+        {"recipe": recipe},
+    )
