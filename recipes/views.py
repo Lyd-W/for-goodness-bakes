d25031cffcd30c8b3ee71e110fccceab81f07e1a
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-01-14 19:11:15 +0000

    Add filtering to remove draft recipes from view and update ordering

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from .models import Recipe
 # Create your views here.
 
 class RecipeList(generic.ListView):
-    queryset = Recipe.objects.all()
+    queryset = Recipe.objects.filter(status=1).order_by("created_on")
     context_object_name = "recipes"
     template_name = "recipes/index.html"
     paginate_by = 6
