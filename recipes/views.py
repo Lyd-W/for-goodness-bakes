9ddfb9c5a0de0542c15f969c355ce0a234a1408b
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-01-17 18:00:52 +0000

    Set up commenting

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Recipe
+from .forms import CommentForm
 
 # Create your views here.
 class RecipeList(generic.ListView):
@@ -12,9 +14,30 @@ class RecipeList(generic.ListView):
 def recipe_detail(request, slug):
     queryset = Recipe.objects.filter(status=1)
     recipe = get_object_or_404(queryset, slug=slug)
+    comments = recipe.comments.filter(approved=True).order_by("-created_on")
+    comment_count = recipe.comments.filter(approved=True).count()
+    
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.comment_on = recipe
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Your comment has been successfully submitted and is awaiting approval'
+            )
+        
+    comment_form = CommentForm()
     
     return render(
         request,
         "recipes/recipe_detail.html",
-        {"recipe": recipe},
+        {
+            "recipe": recipe,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+         },
     )
