8c7f7b4ae85873f86fd97980ffe7b706de03e4c2
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-01-22 21:13:27 +0000

    Style about page and image layout

--- about/admin.py
+++ about/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import About, Contact
 
+
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
 
@@ -20,7 +21,8 @@ class AboutAdmin(SummernoteModelAdmin):
     summernote_fields = (
         'content',
     )
-    
+
+
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
 
--- about/forms.py
+++ about/forms.py
@@ -10,3 +10,10 @@ class ContactForm(forms.ModelForm):
                   'email',
                   'message',
                 )
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'message': forms.Textarea(attrs={
+              'placeholder': 'Write your message here',
+            })
+        }
